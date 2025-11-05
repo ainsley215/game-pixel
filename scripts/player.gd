0efb31e7ebfd8957fb35ebed93befae3b40af640
ainsley215 <ainsley02121@gmail.com>
2025-11-05 19:18:01 +0700

    add combat system

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,11 @@
 extends CharacterBody2D
 
+var skeleton_inattack_range = false
+var skeleton_attack_cooldown = true
+var health = 100
+var player_alive = true
+var attack_ip = false
+
 const speed = 100
 var current_dir = "none"
 
@@ -8,6 +14,14 @@ func _ready():
 
 func _physics_process(delta):
 	player_movement(delta)
+	skeleton_attack()
+	attack()
+	
+	if health <= 0:
+		player_alive = false # add end screen
+		health = 0
+		print("player has been killed")
+		self.queue_free()
 
 func player_movement(delta):
 	
@@ -47,25 +61,96 @@ func play_anim(movement):
 		if movement == 1:
 			anim.play("walk_right")
 		elif movement == 0:
-			anim.play("idle_right")
+			if attack_ip == false:
+				anim.play("idle_right")
 			
 	if dir == "left":
 		anim.flip_h = false
 		if movement == 1:
 			anim.play("walk_left")
 		elif movement == 0:
-			anim.play("idle_left")
+			if attack_ip == false:
+				anim.play("idle_left")
 			
 	if dir == "down":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("walk_down")
 		elif movement == 0:
-			anim.play("idle_down")
+			if attack_ip == false:
+				anim.play("idle_down")
 			
 	if dir == "up":
 		anim.flip_h = true
 		if movement == 1:
 			anim.play("walk_up")
 		elif movement == 0:
-			anim.play("idle_up")
+			if attack_ip == false:
+				anim.play("idle_up")
+
+func player():
+	pass
+
+func _on_player_hitbox_body_entered(body):
+	if body.has_method("skeleton"):
+		skeleton_inattack_range = true
+
+func _on_player_hitbox_body_exited(body):
+	if body.has_method("skeleton"):
+		skeleton_inattack_range = false
+
+func skeleton_attack():
+	if skeleton_inattack_range and skeleton_attack_cooldown == true:
+		health = health - 10
+		skeleton_attack_cooldown = false
+		$attack_cooldown.start()
+		print(health)
+
+func _on_attack_cooldown_timeout():
+	skeleton_attack_cooldown = true
+	
+func attack():
+	var dir = current_dir
+	
+	# pakai flip_h
+	#if Input.is_action_just_pressed("attack"):
+		#global.player_current_attack = true
+		#attack_ip = true
+		#if dir == "right":
+			#$AnimatedSprite2D.flip_h = false
+			#$AnimatedSprite2D.play("attack_right")
+			#$deal_attack_timer.start()
+		#if dir == "left":
+			#$AnimatedSprite2D.flip_h = true
+			#$AnimatedSprite2D.play("attack_left")
+			#$deal_attack_timer.start()
+		#if dir == "down":
+			#$AnimatedSprite2D.play("attack_down")
+			#$deal_attack_timer.start()
+		#if dir == "up":
+			#$AnimatedSprite2D.play("attack_up")
+			#$deal_attack_timer.start()
+	
+	# gak pakai flip_h
+	if Input.is_action_just_pressed("attack"):
+		global.player_current_attack = true
+		attack_ip = true
+
+		var anim = $AnimatedSprite2D
+
+		match dir:
+			"right":
+				anim.play("attack_right")
+			"left":
+				anim.play("attack_left")
+			"down":
+				anim.play("attack_down")
+			"up":
+				anim.play("attack_up")
+
+		$deal_attack_timer.start()
+
+func _on_deal_attack_timer_timeout():
+	$deal_attack_timer.stop()
+	global.player_current_attack = false
+	attack_ip = false
--- scripts/skeleton.gd
+++ scripts/skeleton.gd
@@ -4,10 +4,15 @@ var speed = 40
 var player_chase = false
 var player = null
 var last_dir = "down"
-
 var last_flip = false
+var health = 100
+var player_inattack_zone = false
+var can_take_damage = true
 
 func _physics_process(delta):
+	
+	deal_with_damage()
+	
 	#if player_chase:
 		#position += (player.position - position)/speed
 		#
@@ -98,3 +103,27 @@ func _on_detection_area_body_entered(body):
 func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
+
+func skeleton():
+	pass
+
+func _on_skeleton_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_inattack_zone = true
+
+func _on_skeleton_hitbox_body_exited(body):
+	if body.has_method("player"):
+		player_inattack_zone = false
+
+func deal_with_damage():
+	if player_inattack_zone and global.player_current_attack == true:
+		if can_take_damage == true:
+			health = health - 20
+			$take_damage_cooldown.start()
+			can_take_damage = false
+			print("skeleton health = ", health)
+			if health <= 0:
+				self.queue_free()
+
+func _on_take_damage_cooldown_timeout():
+	can_take_damage = true
